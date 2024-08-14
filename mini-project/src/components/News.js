a0654c4549557ce7a508e3b2dde965336f6724fc
Paras Tuteja <parastuteja2000@gmail.com>
2024-08-14 16:10:09 +0530

    Updated

--- mini-project/src/components/News.js
+++ mini-project/src/components/News.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+export default class News extends Component {
+  constructor(){
+    super();
+    console.log("Construcior from news")
+  }
+  render() {
+    return (
+      <>
+      This is a component of news
+      <div className='container my-3'>
+          <h2>NewsHub- Top Headlines</h2>
+          <div className='row my-4'>
+              <div className='col-md-3'><NewsItem title='title1' description='desc1' imageurl="https://images.pexels.com/photos/2872418/pexels-photo-2872418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/></div>
+              <div className='col-md-3'><NewsItem title='title2' description='desc2' imageurl="https://images.pexels.com/photos/2872418/pexels-photo-2872418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/></div>
+              <div className='col-md-3'><NewsItem title='title3' description='desc3' imageurl="https://images.pexels.com/photos/2872418/pexels-photo-2872418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/></div>
+              <div className='col-md-3'><NewsItem title='title4' description='desc4' imageurl="https://images.pexels.com/photos/2872418/pexels-photo-2872418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/></div>
+              <div className='col-md-3'><NewsItem title='title5' description='desc5' imageurl="https://images.pexels.com/photos/2872418/pexels-photo-2872418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/></div>
+          </div>
 
-function News() {
-  return (
-    <>
-    This is a component of news
-    <div className='container my-3'>
-        <h2>NewsHub- Top Headlines</h2>
-        <div className='row'>
-            <div className='col-md-3'><NewsItem title='title1' description='desc1'/></div>
-            <div className='col-md-3'><NewsItem title='title2' description='desc2'/></div>
-            <div className='col-md-3'><NewsItem title='title3' description='desc3'/></div>
-            <div className='col-md-3'><NewsItem title='title4' description='desc4'/></div>
-            <div className='col-md-3'><NewsItem title='title5' description='desc5'/></div>
-        </div>
-    
-    
-    </div>
-    </>
-  )
+      </div>
+      </>
+    )
+  }
 }
-
-export default News
--- mini-project/src/components/NewsItem.js
+++ mini-project/src/components/NewsItem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
   
   render() {
-    let {title, description}= this.props;
+    let {title, description, imageurl}= this.props;
     return (
         <>
   
-        <div class="card">
-        <img src="..." class="card-img-top" alt="..."/>
+        <div className="card my-3">
+        <img src={imageurl} class="card-img-top" alt=""/>
         <div class="card-body">
           <h5 class="card-title">{title}</h5>
           <p class="card-text">{description}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-primary btn-sm">Read More</a>
         </div>
       </div>
       </>
